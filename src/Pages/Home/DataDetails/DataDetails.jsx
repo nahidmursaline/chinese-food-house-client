9b2e53db1add1bf8444298b4237db643033312e3
nahidmursaline <nahid15-12408@diu.edu.bd>
2023-05-04 22:37:03 +0600

    design

--- src/Pages/Home/DataDetails/DataDetails.jsx
+++ src/Pages/Home/DataDetails/DataDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-import { Card } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const DataDetails = () => {
     const details = useLoaderData();
-    const {name,picture} = details;
+    const {name,picture, bio, recipes, likes, years_of_experience, num_recipes,} = details;
     
     console.log(details)
     
@@ -14,20 +14,26 @@ const DataDetails = () => {
    
     return (
         <div>
-            <h1>this is details</h1>
+            <h1 className='text-center mt-5'>Chef's details</h1>
             
             
-            <Card className='margin'>
-        <Card.Img variant="top" src={picture} />
+          <Container className='m-5 mx-auto'>
+          <Card className='margin'>
+        <Card.Img className='mx-auto' style={{height: 600, width: 700}} variant="top" src={picture} />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>
-            
+            <p>Bio: {bio}</p>
+            <p>Likes: {likes}</p>
+            <p>Years of Experience: {years_of_experience}</p>
+            <p>Number of Recipies: {num_recipes}</p>
+            <p></p>
             
           </Card.Text>
           
         </Card.Body>
       </Card>
+          </Container>
         </div>
     );
 };
